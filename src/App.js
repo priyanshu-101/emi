4ddd5cc6801d9664d8846c12b37123092af462dd
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2024-09-07 08:36:25 +0530

    remove unused route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EMIForm from './components/EmiForm';
-import ResultPage from './components/ResultPage';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<EMIForm />} />
-        <Route path="/results" element={<ResultPage />} />
       </Routes>
     </Router>
   );
